3780e8ae80c2a53a1cb5bfa82d0b42d13188da7f
lucperkins <lucperkins@gmail.com>
2019-04-19 16:46:25 -0700

    Change file path for write operation
    Signed-off-by: lucperkins <lucperkins@gmail.com>

--- scripts/generate-pages.rb
+++ scripts/generate-pages.rb
@@ -4,7 +4,7 @@ File.open('./data/projects.yaml', 'r') do |f|
   projects = YAML.load(f)
   projects.each do |p|
     metadata = "---\ntitle: #{p['name']}\n---\n"
-    dest_file = "./content/projects/#{p['tag']}.md"
+    dest_file = "content/projects/#{p['tag']}.md"
 
     File.open(dest_file, 'w') do |f|
       f.write(metadata)
